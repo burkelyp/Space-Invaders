1da4271b0d75f7edb29ae2a6bf96b004494a976a
tassonem <tassonem@oregonstate.edu>
2025-05-13 11:01:33 -0500

    Changes made too work with SDL2

--- loadrom.cpp
+++ loadrom.cpp
@@ -1,13 +1,17 @@
 #include "loadrom.h"
-#include <sys/stat.h>
-
+#include <iostream>
 #pragma warning(disable:4996)
 
-
-void loadROM(const char* path, State8080* state) {
+/**
+* Loads the ROM into the emulated Intel8080 CPU.
+*
+* @param path C-style string representing the path to the ROM file.
+* @param state Pointer to a State8080 struct holding the CPU state and memory.
+*/
+void loadROM(const char* path, State8080* state, uint16_t offset) {
     FILE* ptr = std::fopen(path, "rb");
     if (ptr == nullptr) {
-        std::cerr << "File pointer is null." << std::endl;
+        std::cerr << "File pointer is null." << path << std::endl;
         exit(1);
     }
 
@@ -17,7 +21,7 @@ void loadROM(const char* path, State8080* state) {
         exit(1);
     }
 
-    size_t result = fread(state->memory, 1, statbuf.st_size, ptr);
+    size_t result = fread(state->memory + offset, 1, statbuf.st_size, ptr);
     if (result != statbuf.st_size) {
         std::cerr << "Failed to read file. Reading error or EOF." << path << std::endl;
         exit(1);
--- loadrom.h
+++ loadrom.h
@@ -13,6 +13,6 @@
 * @param path C-style string representing the path to the ROM file.
 * @param state Pointer to a State8080 struct holding the CPU state and memory.
 */
-void loadROM(const char* path, State8080* state);
+void loadROM(const char* path, State8080* state, uint16_t offset);
 
 #endif
